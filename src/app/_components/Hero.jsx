c29f7fc106d5f19006f04e4cb24db322461fbf5d
Ханхүү Сүнчин <26lp99264675@administrators-iMac-4.local>
2026-09-10 13:05:57 +0800

    dkghggh

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -68,7 +68,7 @@ export const Hero = () => {
           Elphaba, a misunderstood young woman because of her green skin, and
           Glinda, a popular girl, become friends at Shiz University in the Land
           of Oz. After an encounter with the Wonderful Wizard of Oz, their
-          friendship reaches a crossroads.
+          friendship reaches a crossroads ghghghghghghghghghhgg.
         </p>
 
         <div>
